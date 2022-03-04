cc3413c9cbb4ccfaa8420163f265cfd21e32079d
Egor <bodnariuc.egor@outlook.com>
2022-03-04 15:51:16 +0200

    header and services block

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const sliderMain = document.querySelector('.swiper-main');
+const sliderNav = document.querySelector('.swiper-nav');
+
+  const swiperNav = new Swiper('.swiper-nav', {
+    slidesPerView: 3,
+    direction: 'vertical',
+    freeMode: true,
+    speed: 400,
+    spaceBetween: 5,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+      },
+  });
+
+  const swiperMain = new Swiper('.swiper-main', {
+    slidesPerView: 1,
+    speed: 400,
+    spaceBetween: 100,
+    thumbs: {
+        swiper: swiperNav,
+      },
+  });
+
