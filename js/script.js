b426a98505d9c2bea0e9f9543d83c1ea681f080a
unknown <bodnariuc.egor@outlook.com>
2022-03-06 15:57:21 +0200

    Minor fixes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Swiper slider functionality
 const sliderMain = document.querySelector('.swiper-main');
 const sliderNav = document.querySelector('.swiper-nav');
 
@@ -22,3 +23,44 @@ const sliderNav = document.querySelector('.swiper-nav');
       },
   });
 
+
+  // Tabs show hide functionality
+
+  const show = function (elem) {
+    elem.style.display = 'block';
+  };
+  
+  const hide = function (elem) {
+    elem.style.display = 'none';
+  };
+  
+  const toggle = function (elem) {
+  
+    // If the element is visible, hide it
+    if (window.getComputedStyle(elem).display === 'block') {
+      hide(elem);
+      return;
+    }
+  
+    // Otherwise, show it
+    show(elem);
+  
+  };
+  
+  // Listen for click events
+  document.addEventListener('click', function (event) {
+  
+    // Make sure clicked element is our toggle
+    if (!event.target.classList.contains('toggle')) return;
+  
+    // Prevent default link behavior
+    event.preventDefault();
+  
+    // Get the content
+    let content = document.querySelector(event.target.hash);
+    if (!content) return;
+  
+    // Toggle the content
+    toggle(content);
+  
+  }, false);
